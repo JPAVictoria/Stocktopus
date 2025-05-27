a13e4b969eaea0fe61f9c187296ecb7e4354b3ca
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-05-27 18:08:27 +0800

    update-commit

--- app/components/InventoryCharts.jsx
+++ app/components/InventoryCharts.jsx
@@ -7,26 +7,38 @@ export default function InventoryCharts({ pieData, lineData }) {
       <div className="mt-10 mb-8">
         <div className="grid grid-cols-3 gap-6 w-300 mx-auto">
           <div className="bg-white border border-[#2D2D2D]/25 rounded-lg p-6">
-            <p className="font-regular text-[#333333] text-[14px] mb-1">Company User Count:</p>
+            <p className="font-regular text-[#333333] text-[14px] mb-1">
+              Company User Count:
+            </p>
             <p className="text-lg text-[#333333] font-bold mb-1">20k</p>
-            <p className="text-[#333333]/75 text-[12.5px] font-regular mb-5">Last 30 days</p>
+            <p className="text-[#333333]/75 text-[12.5px] font-regular mb-5">
+              Last 30 days
+            </p>
             <LineChart {...lineData} height={120} colors={["#00C853"]} />
           </div>
 
-          <div className="bg-white p-4  border border-[#2D2D2D]/25 rounded-lg text-center">
-            <p className="font-medium">Most Stocked Products</p>
-            <p className="text-sm">Top 3 by Quantity</p>
-            <ul className="mt-2 space-y-1 font-semibold">
+          <div className="bg-white p-6 border border-[#2D2D2D]/25 rounded-lg text-center">
+            <p className="font-regular text-[14px] text-[#3d3d3d] mb-1">
+              Most Stocked Products
+            </p>
+            <p className="text-[#333333]/75 text-[12.5px]">Top 5 by Quantity</p>
+            <ul className="space-y-1 font-semibold mt-5">
               <li>Keyboard Warrior</li>
+              <li>Apple Wine</li>
+              <li>Juice ko po</li>
               <li>Apple Wine</li>
               <li>Juice ko po</li>
             </ul>
           </div>
 
-          <div className="bg-white p-4  border border-[#2D2D2D]/25 rounded-lg">
-            <p className="font-medium">Active Suppliers:</p>
-            <p className="text-lg font-bold">20k</p>
-            <p className="text-sm">Last 30 days</p>
+          <div className="bg-white p-6 border border-[#2D2D2D]/25 rounded-lg">
+            <p className="font-regular text-[#333333] text-[14px]">
+              Active Suppliers:
+            </p>
+            <p className="text-lg text-[#333333] font-bold mb-1">20k</p>
+            <p className="text-[#333333]/75 text-[12.5px] font-regular mb-5">
+              Last 30 days
+            </p>
             <LineChart {...lineData} height={120} colors={["#00C853"]} />
           </div>
         </div>
